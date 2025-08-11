b093e49eca1044eb54dc0bdcabc64d90201eae3c
dev-cyan <zyos1@naver.com>
2025-08-11 19:17:57 +0900

    feat: Member 엔티티 구현

--- src/main/java/capssungzzang/_dayscottonball/domain/member/domain/entity/Member.java
+++ src/main/java/capssungzzang/_dayscottonball/domain/member/domain/entity/Member.java
@@ -1,4 +1,40 @@
 package capssungzzang._dayscottonball.domain.member.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String nickname;
+
+    @Column(nullable = false)
+    private int candy;
+
+    @Column(nullable = false)
+    private int level; // 1~10(임시)
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 1)
+    private Band band; // A~E(임시)
+
+    public enum Band { A, B, C, D, E }
+
+    @Builder
+    public Member(String nickname, int candy, int difficulty, Integer level, Band band) {
+        this.nickname = nickname;
+        this.candy = candy;
+        this.level = level;
+        this.band  = band;
+    }
+
 }
