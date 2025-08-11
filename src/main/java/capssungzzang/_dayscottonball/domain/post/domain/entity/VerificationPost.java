be1f7af58231a9f77b5c4a8daa6f36886c1cdf3d
dev-cyan <zyos1@naver.com>
2025-08-11 20:32:53 +0900

    chore: 주석 수정

--- src/main/java/capssungzzang/_dayscottonball/domain/post/domain/entity/VerificationPost.java
+++ src/main/java/capssungzzang/_dayscottonball/domain/post/domain/entity/VerificationPost.java
@@ -6,7 +6,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-// 자식: 인증글 (부모 PK를 그대로 사용)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
