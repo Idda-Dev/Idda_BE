a08212fe6d7bf8333eba21b73a92f287b1ecfcc3
dev-cyan <zyos1@naver.com>
2025-08-17 19:04:28 +0900

    feat: MissionGenerateResponse 추가

---
+++ src/main/java/capssungzzang/_dayscottonball/domain/mission/dto/MissionGenerateResponse.java
@@ -0,0 +1,11 @@
+package capssungzzang._dayscottonball.domain.mission.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MissionGenerateResponse {
+    private String content;
+    private String comment;
+}
